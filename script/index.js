975564feeba1dae32caf3196fc1b89f59ea5d6cb
Follow-Pathan <138471027+Follow-Pathan@users.noreply.github.com>
2024-02-26 09:02:09 -0500

    Delete script directory

--- script/index.js
+++
@@ -1,8 +0,0 @@
-const imageText=document.getElementById(imageText);
-const beaker = document.getElementById("beaker");
-
-beaker.addEventListener("mouseenter",function(){
-    if(imageText){
-        imageText.style.display="block"
-    }
-});
